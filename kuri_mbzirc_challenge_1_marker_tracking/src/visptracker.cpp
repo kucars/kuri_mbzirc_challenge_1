5c903a97102898428afa543911ba3e58b96fe8a4
Mohammed S. Khoory <mohammed.khoory@mbrsc.ae>
2017-02-28 11:56:16 +0400

    Tweak parameters and model file slightly. Gives better tracking performance.

--- kuri_mbzirc_challenge_1_marker_tracking/src/visptracker.cpp
+++ kuri_mbzirc_challenge_1_marker_tracking/src/visptracker.cpp
@@ -100,8 +100,8 @@ int main(int argc, char ** argv)
 	TrackMarkerModel * tmm = new TrackMarkerModel(camSizeX, camSizeY);
 	tmm->setMaskSize(5);
 	tmm->setMaskNumber(180);
-	tmm->setRange(8);
-	tmm->setThreshold(10000);
+	tmm->setRange(30);
+	tmm->setThreshold(30000);
 	tmm->setMu1(0.5);
 	tmm->setMu2(0.5);
 	tmm->setSampleStep(4);
